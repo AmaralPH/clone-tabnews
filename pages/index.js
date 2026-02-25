6e0fd17330529bd4d77f0922d28def6e9f11c450
Pedro Amaral <amaralph1995@gmail.com>
2026-02-25 16:03:41 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+function Home() {
+    return <>
+    <h1>Esse foi meu primeiro deploy na versel.</h1>
+    <p>Fico me perguntando como eu faria o deploy de uma aplicação que não utiliza next</p>
+    </>
+}
+
+export default Home;
