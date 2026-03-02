3d80f609c21b846896c973c407e2e34d6d73b891
Pedro Amaral <amaralph1995@gmail.com>
2026-03-02 16:57:28 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 function Home() {
-    return <>
-    <h1>Esse foi meu primeiro deploy na versel.</h1>
-    <p>Fico me perguntando como eu faria o deploy de uma aplicação que não utiliza next</p>
+  return (
+    <>
+      <h1>Esse foi meu primeiro deploy na versel.</h1>
+      <p>
+        Fico me perguntando como eu faria o deploy de uma aplicação que não
+        utiliza next
+      </p>
     </>
+  );
 }
 
 export default Home;
